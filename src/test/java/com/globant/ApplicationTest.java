42f6664f3deb3a353274c29ca5af3ae9ce04897f
Belen <belen.scaffardi@gmail.com>
2025-03-26 20:46:26 -0300

    eliminar datos

--- src/test/java/com/globant/ApplicationTest.java
+++ src/test/java/com/globant/ApplicationTest.java
@@ -13,10 +13,18 @@ public class ApplicationTest {
     }
 
     @Test
-    public void test1() {
+    public void verificaConexion() {
         given().when().get("https://67e487d02ae442db76d4b3ea.mockapi.io/api/v1/usuarios")
-                .then().statusCode(200);
+                .then().statusCode(200).log().body();
 
+    }
+
+    @Test
+    public void eliminarDatos(){
+        given().when().delete("https://67e487d02ae442db76d4b3ea.mockapi.io/api/v1/usuarios")
+        .then().statusCode(200);
+
+       verificaConexion();
     }
 
 }
