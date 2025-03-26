936e5203047ea5a2cacf5e47a6476c9d267f4570
Belen <belen.scaffardi@gmail.com>
2025-03-26 20:33:16 -0300

    test evaluar conexion

--- src/test/java/com/globant/ApplicationTest.java
+++ src/test/java/com/globant/ApplicationTest.java
@@ -3,6 +3,7 @@ package com.globant;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.restassured.RestAssured.*;
 
 public class ApplicationTest {
 
@@ -10,4 +11,12 @@ public class ApplicationTest {
     void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
+    @Test
+    public void test1() {
+        given().when().get("https://67e487d02ae442db76d4b3ea.mockapi.io/api/v1/usuarios")
+                .then().statusCode(200);
+
+    }
+
 }
